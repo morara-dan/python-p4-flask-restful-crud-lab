fad35d810a7aeb1803c4a82bc0346364a546cf05
dancan <dancan.morara@student.moringaschool.com>
2025-06-22 11:42:22 +0300

    CRUD

--- server/app.py
+++ server/app.py
@@ -47,6 +47,26 @@ class PlantByID(Resource):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
 
+    def patch(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if not plant:
+            return make_response({"error": "Plant not found"}, 404)
+        data = request.get_json()
+        for attr in data:
+            if hasattr(plant, attr):
+                setattr(plant, attr, data[attr])
+        db.session.add(plant)
+        db.session.commit()
+        return make_response(plant.to_dict(), 200)
+
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if not plant:
+            return make_response({"error": "Plant not found"}, 404)
+        db.session.delete(plant)
+        db.session.commit()
+        return make_response('', 204)
+
 
 api.add_resource(PlantByID, '/plants/<int:id>')
 
